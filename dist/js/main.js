5c9d65a4b335c9668a26677492013077c03d5270
wwwooo <malinovsvska@gmail.com>
2016-11-19 17:40:20 +0200

    update ajax

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,84 @@
-$(function(){$("#h-it-w__section-1").slick({initialSlide:0}),$("#h-it-w__section-2").slick({initialSlide:1}),$("#h-it-w__section-3").slick({initialSlide:2});var e=document.getElementById("search"),t=document.getElementById("search-btn"),i=document.getElementById("grid"),n=new XMLHttpRequest,s=function(e,t,i){i=i||15,n.open("GET","http://api.pixplorer.co.uk/image?word="+e+"&amount="+i),n.onreadystatechange=function(){200===n.status&&4===n.readyState?t(JSON.parse(n.response)):200!==n.status&&console.log("false request",n.status)},n.send()};t.addEventListener("click",function(){s(e.value,function(e){i.innerHTML=tmpl("item_tmpl",{data:e.images}),$(".discover-ideas__grid").imagesLoaded(function(){$(".discover-ideas__grid").masonry({itemSelector:".discover-ideas__item",columnWidth:".discover-ideas__item",percentPosition:!0})})})}),t.dispatchEvent(new Event("click"))});
+$(function() {
+    $('#h-it-w__section-1').slick({
+        initialSlide: 0
+    });
+
+    $('#h-it-w__section-2').slick({
+        initialSlide: 1
+    });
+
+    $('#h-it-w__section-3').slick({
+        initialSlide: 2
+    });
+
+    var search = document.getElementById('search');
+    var submit = document.getElementById('search-btn');
+    var content = document.getElementById('grid');
+    var requestTag = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
+
+    var getRandomInt = function(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    };
+
+    var randomTag = function(callback) {
+        requestTag.open('GET', 'https://pixabay.com/api/?key=2344285-2757d4dcb4a1174d53dd52d34&per_page=25', false);
+        requestTag.onreadystatechange = function() {
+            if (requestTag.status === 200 && requestTag.readyState === 4) {
+                callback(JSON.parse(requestTag.response));
+                console.log('true requestTag', requestTag.status);
+            } else if (requestTag.status !== 200) {
+                console.log('false requestTag', requestTag.status);
+            }
+        };
+        requestTag.send();
+    };
+
+    var makeRequest = function(tag, callback, limit) {
+        limit = limit || 10;
+        console.log('tag: ' + tag);
+        request.open('GET', 'https://pixabay.com/api/?key=2344285-2757d4dcb4a1174d53dd52d34&q=' + tag + '&per_page=' + limit + '&orientation=vertical');
+        request.onreadystatechange = function() {
+            if (request.status === 200 && request.readyState === 4) {
+                callback(JSON.parse(request.responseText));
+                console.log('true request', request.status);
+            } else if (request.status !== 200) {
+                console.log('false request', request.status);
+            }
+        };
+        request.send();
+    };
+
+    submit.addEventListener('click', function() {
+        console.log('first search.value: ', search.value);
+        if (search.value === '') {
+            randomTag(function(data) {
+                var data = data.hits;
+                var str = '';
+                for (var i = 0; i < data.length; i++) {
+                    str += data[i].tags;
+                }
+                var str = str.split(', ');
+                console.log('str: ', str);
+                search.value = str[getRandomInt(0, str.length - 1)];
+                console.log('tag: ', search.value);
+            });
+            console.log('end randomTag request');
+        }
+
+        makeRequest(search.value, function(data) {
+            content.innerHTML = tmpl('item_tmpl', {data: data.hits});
+            search.value = '';
+            $('.discover-ideas__grid').imagesLoaded(function() {
+                $('.discover-ideas__grid').masonry({
+                    itemSelector: '.discover-ideas__item',
+                    columnWidth: '.discover-ideas__item',
+                    percentPosition: true
+                });
+            });
+            console.log('end makeRequest\n__________________');
+        });
+    });
+
+    submit.dispatchEvent(new Event('click'));
+});
--- src/js/main.js
+++ src/js/main.js
@@ -14,15 +14,23 @@ $(function() {
     var search = document.getElementById('search');
     var submit = document.getElementById('search-btn');
     var content = document.getElementById('grid');
+    var request1 = new XMLHttpRequest();
     var request = new XMLHttpRequest();
+
+    var request1 = function(tag) {
+        request1.open('GET', 'https://wordsapiv1.p.mashape.com/words/?random=true');
+        request1.onreadystatechange = function() {
+            JSON.parse(request1.responseText.word);
+        };
+        request1.send();
+    };
 
     var makeRequest = function(tag, callback, limit) {
         limit = limit || 15;
-
-        request.open('GET', 'http://api.pixplorer.co.uk/image?word=' + tag + '&amount=' + limit);
+        request.open('GET', 'https://pixabay.com/api/?key=2344285-2757d4dcb4a1174d53dd52d34&q=' + tag + '&per_page=' + limit + '&orientation=vertical');
         request.onreadystatechange = function() {
             if (request.status === 200 && request.readyState === 4) {
-                callback(JSON.parse(request.response));
+                callback(JSON.parse(request.responseText));
             } else if (request.status !== 200) {
                 console.log('false request', request.status);
             }
@@ -31,8 +39,11 @@ $(function() {
     };
 
     submit.addEventListener('click', function() {
+        if (!search.value) {
+            request1();
+        }
         makeRequest(search.value, function(data) {
-            content.innerHTML = tmpl('item_tmpl', {data: data.images});
+            content.innerHTML = tmpl('item_tmpl', {data: data.hits});
 
             $('.discover-ideas__grid').imagesLoaded(function() {
                 $('.discover-ideas__grid').masonry({
