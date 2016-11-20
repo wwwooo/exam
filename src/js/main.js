474b9f66031df0fe0f4f92ab78527a59a32c73ed
wwwooo <malinovsvska@gmail.com>
2016-11-20 10:38:42 +0200

    update discover-ideas-320

--- src/js/main.js
+++ src/js/main.js
@@ -14,45 +14,67 @@ $(function() {
     var search = document.getElementById('search');
     var submit = document.getElementById('search-btn');
     var content = document.getElementById('grid');
-    var request1 = new XMLHttpRequest();
-    var request = new XMLHttpRequest();
+    var requestTags = new XMLHttpRequest();
+    var requestImg = new XMLHttpRequest();
+    var tags = [];
 
-    var request1 = function(tag) {
-        request1.open('GET', 'https://wordsapiv1.p.mashape.com/words/?random=true');
-        request1.onreadystatechange = function() {
-            JSON.parse(request1.responseText.word);
-        };
-        request1.send();
+    var getRandomInt = function(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     };
 
-    var makeRequest = function(tag, callback, limit) {
-        limit = limit || 15;
-        request.open('GET', 'https://pixabay.com/api/?key=2344285-2757d4dcb4a1174d53dd52d34&q=' + tag + '&per_page=' + limit + '&orientation=vertical');
-        request.onreadystatechange = function() {
-            if (request.status === 200 && request.readyState === 4) {
-                callback(JSON.parse(request.responseText));
-            } else if (request.status !== 200) {
-                console.log('false request', request.status);
+    var makeRequestTags = function(callback) {
+        requestTags.open('GET', 'https://pixabay.com/api/?key=2344285-2757d4dcb4a1174d53dd52d34&per_page=100', false);
+        requestTags.onreadystatechange = function() {
+            if (requestTags.status === 200 && requestTags.readyState === 4) {
+                callback(JSON.parse(requestTags.response));
+            } else if (requestTags.status !== 200) {
+                console.log('false requestTag', requestTags.status);
             }
         };
-        request.send();
+        requestTags.send();
+    };
+
+    var makeRequestImg = function(tag, callback, limit) {
+        limit = limit || 7;
+        requestImg.open('GET', 'https://pixabay.com/api/?key=2344285-2757d4dcb4a1174d53dd52d34&q=' + tag + '&per_page=' + limit + '&orientation=vertical');
+        requestImg.onreadystatechange = function() {
+            if (requestImg.status === 200 && requestImg.readyState === 4) {
+                callback(JSON.parse(requestImg.responseText));
+            } else if (requestImg.status !== 200) {
+                console.log('false request', requestImg.status);
+            }
+        };
+        requestImg.send();
     };
 
     submit.addEventListener('click', function() {
-        if (!search.value) {
-            request1();
+        if (search.value === '') {
+            search.value = tags[getRandomInt(0, tags.length - 1)];
         }
-        makeRequest(search.value, function(data) {
-            content.innerHTML = tmpl('item_tmpl', {data: data.hits});
 
-            $('.discover-ideas__grid').imagesLoaded(function() {
-                $('.discover-ideas__grid').masonry({
-                    itemSelector: '.discover-ideas__item',
-                    columnWidth: '.discover-ideas__item',
-                    percentPosition: true
+        makeRequestImg(search.value, function(data) {
+            if (data.totalHits) {
+                content.innerHTML = tmpl('item_tmpl', {data: data.hits});
+                $('.discover-ideas__grid').imagesLoaded(function() {
+                    $('.discover-ideas__grid').masonry({
+                        itemSelector: '.discover-ideas__item',
+                        columnWidth: '.discover-ideas__item',
+                        percentPosition: true
+                    });
                 });
-            });
+            } else {
+
+            }
         });
+    });
+
+    makeRequestTags(function(data) {
+        var data = data.hits;
+        var str = '';
+        for (var i = 0; i < data.length; i++) {
+            str += data[i].tags + ', ';
+        }
+        tags = str.split(', ');
     });
 
     submit.dispatchEvent(new Event('click'));
